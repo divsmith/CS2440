e2dff2c02760a38d3fcec61448f9fbbe3f7ad4d5
Parker Smith <parker@parkersmith.us>
2016-03-11 17:38:19 -0700

    Add database connection for assignment

---
+++ MySQL-Form/database.php
@@ -0,0 +1,17 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $password = 'databasepassword';
+    $database_name = 'Library';
+
+    $connection = mysql_connect($host, $user, $password);
+
+    if (!$connection) {
+        die('Unable to connect to database [' . mysql_error() . ']');
+    }
+
+    $database = mysql_select_db($database_name, $connection);
+
+    if (!$database) {
+        die('Can\'t use root at : ' . mysql_error());
+    }
